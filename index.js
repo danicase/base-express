0e10de8cec9a72cee4c5b9d11c88fe2d4efd3710
Daniel Caseri <daniel.caseri@itv.com>
2021-06-28 10:13:51 +0100

    added logger

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ app.use('/*', (req, res) => {
   res.send(`made a call at ${req.path}`);
 });
 
+const logger = function (req, res, next) {
+  console.log(req.url);
+  next(); // Passing the request to the next handler in the stack.
+};
+app.use(logger);
+
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
