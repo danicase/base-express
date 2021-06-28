842399145b424acb95e14e8f9f710235c0d576ee
Daniel Caseri <daniel.caseri@itv.com>
2021-06-28 10:21:10 +0100

    moved logger

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const app = require('express')();
 const PORT = 4141;
-app.use('/*', (req, res) => {
-  res.send(`made a call at ${req.path}`);
-});
 
 const logger = function (req, res, next) {
   console.log(req.url);
   next(); // Passing the request to the next handler in the stack.
 };
 app.use(logger);
+
+app.use('/*', (req, res) => {
+  res.send(`made a call at ${req.path}`);
+});
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
