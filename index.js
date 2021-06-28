575fe1df45cb20d6bc3d2ef4b471ad5e1e2cb26b
Daniel Caseri <daniel.caseri@itv.com>
2021-06-28 12:03:07 +0100

    improved

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const PORT = 8080;
 const app = express();
 const healthCheck = express();
 
-healthCheck.get('/health', (req, res) => {
-  res.send(JSON.stringify({ status: 'ok' }));
-});
-healthCheck.listen(8081);
-
 const logger = function (req, res, next) {
   console.log(req.url);
   next(); // Passing the request to the next handler in the stack.
 };
 app.use(logger);
+healthCheck.use(logger);
+
+healthCheck.get('/health', (req, res) => {
+  res.send(JSON.stringify({ status: 'ok' }));
+});
 
 app.use('/*', (req, res) => {
   res.send(`made a call at ${req.url}`);
 });
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
+healthCheck.listen(8081, () => console.log('healthCheck done at port 8081'));
