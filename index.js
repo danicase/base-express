1103e3f59f71e2096801b9b6f60a45044a59ef15
Daniel Caseri <daniel.caseri@itv.com>
2021-06-30 15:03:43 +0100

    added test page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const PORT = 8080;
 const app = express();
 const healthCheck = express();
 
 const logger = function (req, res, next) {
   console.log(req.url);
-  next(); // Passing the request to the next handler in the stack.
+  next();
 };
 app.use(logger);
 healthCheck.use(logger);
@@ -14,9 +15,7 @@ healthCheck.get('/health', (req, res) => {
   res.send(JSON.stringify({ status: 'ok' }));
 });
 
-app.use('/*', (req, res) => {
-  res.send(`made a call at ${req.url}`);
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
 healthCheck.listen(8081, () => console.log('healthCheck done at port 8081'));
